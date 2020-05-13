c81436f0ed391e4906cd376fbc20e8f119a9c127
Haven <stackandslash@gmail.com>
2020-05-13 15:09:37 -0700

    makeSlot added, possible opportunity to go back to this point for a variable hour version

---
+++ code.js
@@ -0,0 +1,71 @@
+console.log("Howdi");
+
+var planner = {
+    7: "",
+    8: "",
+    9: "",
+    10: "This is the note for 10 AM",
+    11: "",
+    12: "",
+    13: "",
+    14: "",
+    15: "",
+    16: "",
+    17: "",
+    18: "",
+    19: "",
+    20: "",
+}
+if (!localStorage.getItem("planner")){
+    console.log("It's blank.");
+}
+else{
+    planner = localStorage.getItem("planner");
+}
+
+
+function hourCheck(){
+    return "Time";
+}
+
+//we make a big object tree of a variable, where each hour is defined by number, and contains the string of the notes for that hour. 
+//
+//This will need to be bounced into local storage.
+
+//makeSlot function makes a time slot row and the associated columns, giving the row an ID associated with the hour. It checks the relative time, then sets the class of the textarea element appropriately.
+function makeSlot(hour){
+    var timeClass = "present";
+    var rawTime = moment().format("H");
+    if(rawTime < hour){
+        timeClass = "future";
+    }
+    else if(rawTime > hour){
+        timeClass = "past";
+    }
+    
+    if(rawTime > 12){
+        timeString = (rawTime - 12) + " PM"
+    }
+    else{
+        timeString = (rawTime) + " AM"
+    }
+
+    console.log(timeClass);
+var rowEl = $("<div class='row time-block' id='" + hour + "'>");
+$(".container").append(rowEl);
+
+rowEl.append($("<div class='col-1 hour'> <p>" + timeString + "</p></div>"));
+
+rowEl.append($('<textarea class="col-10 description ' + timeClass + '">' + planner[hour] + '</textarea>'));
+
+rowEl.append($("<button class='col-1 btn saveBtn'>Save</button>"));
+}
+
+
+//the function here will place a time slot for each key in the planner variable.
+makeSlot(10);
+makeSlot(10);
+makeSlot(10);
+
+
+//What if we set up an option to enter the time span you wanted to cover? The parts are already largely in place.
